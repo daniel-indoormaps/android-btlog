4f714ab4b02d61d89bdb3aec008e109297f12267
Yang Chen <mike.c.yang@gmail.com>
2014-08-28 12:11:11 +0800

    Add x60 support

--- lib/log_scan.rb
+++ lib/log_scan.rb
@@ -1,18 +1,55 @@
 module LogScan
+    class Config
+        attr_accessor :vendor
+
+        def initialize
+            @vendor = :mstar
+        end
+
+        def self.inst
+            @inst ||= self.new
+        end
+    end
+
     class Rule
         attr_accessor :sample, :regex, :action
 
-        COMMON_REX = '^\s*\d\d-\d\d\s+\d\d:\d\d:\d\d\.\d\d\d\s+\d+\s+\d+\s+\w\s+'
+        
 
-        def initialize( regex, sample, block )
+        COMMON_REX ={ 
+            :mstar => '^\s*\d\d-\d\d\s+\d\d:\d\d:\d\d\.\d\d\d\s+\d+\s+\d+\s+\w\s+%s:\s+',
+            :qualcomm => '^\s*\d\d-\d\d\s+\d\d:\d\d:\d\d\.\d\d\d\s+\w\/%s\s*\(\s*\d+\):\s+',
+        }
+
+        def self.detect_vendor(line)
+            puts line
+            COMMON_REX.each_pair do |key,value|
+                puts "%s=>%s" % [ key, value % '\w+' ]
+                if Regexp.compile( value % '\w+' ).match( line )
+                     return key
+                end
+            end
+
+            raise "Can't detect vendor"
+        end
+
+        def initialize( tag, regex, sample, block )
             @sample = sample
-            puts COMMON_REX + regex + '$'
-            @regex = Regexp.compile( COMMON_REX + regex + '$' )
+            puts Config.inst.vendor
+            puts COMMON_REX[Config.inst.vendor] % tag + regex + '$'
+            @regex = gen_regexp( Config.inst.vendor, tag, regex )
             @action = block
-
-            if @regex.match( @sample ).nil?
+            if test_match( :mstar, tag, regex ).nil? and test_match( :qualcomm, tag, regex ).nil?
                 raise "%s can't match %s" % [ @regex.to_s, @sample ]
             end
+        end
+
+        def gen_regexp( vendor, tag, regex )
+            Regexp.compile( COMMON_REX[vendor] % tag + regex + '$' )
+        end
+
+        def test_match(vendor, tag, regex)
+            gen_regexp(vendor,tag,regex).match( @sample ).nil?
         end
 
         def match( line )
@@ -25,12 +62,13 @@ module LogScan
     end
 
     module ClassMethods
+
         def rules
             @rules ||= []
         end
 
-        def rule( regex, sample, &block )
-            rules << Rule.new( regex, sample, block )
+        def rule( tag, regex, sample, &block )
+            rules << Rule.new( tag, regex, sample, block )
         end
 
         def match_line( line )
